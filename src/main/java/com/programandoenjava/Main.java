112758bc64d205fb7bacf45c44418912d8ca605a
Four Seagulls <cuatrogabiotas@gmail.com>
2024-01-17 01:12:12 +0100

    Se me olvidaron unas comillas

--- src/main/java/com/programandoenjava/Main.java
+++ src/main/java/com/programandoenjava/Main.java
@@ -1,8 +1,9 @@
 package com.programandoenjava;
-
+import com.programandoenjava.parte1.Parte1;
 public class Main {
 
     public static void main(String[] args) {
-
+        Parte1 p1 = new Parte1();
+        System.out.println(p1.invertirPalabras("Hola mundo"));
     }
 }
--- src/main/java/com/programandoenjava/parte1/Parte1.java
+++ src/main/java/com/programandoenjava/parte1/Parte1.java
@@ -1,7 +1,9 @@
 package com.programandoenjava.parte1;
-
+import java.util.*;
 public class Parte1 {
-
+    StringTokenizer st;
+    ArrayList<String> palabras = new ArrayList<>();
+    String palabraInvertida = "";
     /**
      * Escribe un programa en Java que reciba una cadena de texto y devuelva la
      * misma cadena pero con cada palabra invertida. Por ejemplo, para la entrada "Hola Mundo",
@@ -9,7 +11,17 @@ public class Parte1 {
      * @param cadena cadena de entrada
      * @return cadena invertida
      */
-    public String invertirPalabras(final String cadena) {;
-        return null;
+    public String invertirPalabras(final String cadena) {
+        st = new StringTokenizer(cadena);
+        while(st.hasMoreTokens()){
+            palabras.add(st.nextToken()+" ");
+        }
+        for(String palabra : palabras){
+            for(int i=palabra.length()-1; i>=0; i--){
+                palabraInvertida+= palabra.charAt(i);
+            }
+        }
+
+        return palabraInvertida;
     }
 }
