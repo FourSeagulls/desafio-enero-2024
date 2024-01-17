7e1947bfde78589745de66cfe65499a0b9480aa0
Four Seagulls <cuatrogabiotas@gmail.com>
2024-01-17 15:26:45 +0100

    Parte2 hecha
    En el main arranca todo del tirón.
    Sólo hay que seguir las instrucciones.

--- src/main/java/com/programandoenjava/Main.java
+++ src/main/java/com/programandoenjava/Main.java
@@ -1,9 +1,27 @@
 package com.programandoenjava;
 import com.programandoenjava.parte1.Parte1;
+import com.programandoenjava.parte2.Parte2;
+import java.util.Scanner;
 public class Main {
 
+    static Scanner entrada = new Scanner(System.in);
+    static String frase;
+    static int iniciador = 0;
+
     public static void main(String[] args) {
+
         Parte1 p1 = new Parte1();
-        System.out.println(p1.invertirPalabras("Hola mundo"));
+        System.out.println("Dime la frase que quieres invertir: ");
+        frase = entrada.nextLine();
+        System.out.println("Entrada invertida: " + p1.invertirPalabras(frase));
+
+
+        Parte2 p2 = new Parte2();
+        System.out.println("introduce le número de partida: ");
+        iniciador = entrada.nextInt();
+        p2.agregarNumero(iniciador);
+
+        System.out.println("El resultado de la suma es: " + p2.sumarNumeros());
+        entrada.close();
     }
 }
--- src/main/java/com/programandoenjava/parte2/Parte2.java
+++ src/main/java/com/programandoenjava/parte2/Parte2.java
@@ -1,19 +1,29 @@
 package com.programandoenjava.parte2;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
 /**
  * Implementa en Java que tenga un método
  * para agregar números enteros y otro método para calcular
  * la suma de todos los elementos en la lista.
  */
 public class Parte2 {
-
+    Scanner entrada = new Scanner(System.in);
+    public ArrayList<Integer> listaNum = new ArrayList<>();
+    int num = 0;
     /**
      * Calcula la suma de todos los números enteros.
      *
      * @return La suma de los números enteros.
      */
     public int sumarNumeros() {
-        return 0;
+        int resultado = 0;
+        for(int i=listaNum.size()-1; i>=0; i--){
+            resultado += listaNum.get(i);
+        }
+        return resultado;
     }
 
     /**
@@ -22,5 +32,32 @@ public class Parte2 {
      * @param numero El número entero que se va a agregar.
      */
     public void agregarNumero(int numero) {
+        num = numero;
+        int contador=0;
+        listaNum.add(num);
+        do{
+            try{
+
+                System.out.print("""
+                                   Si ingresas 0 esto terminará.
+                                   Dame otro valor entero para agregar: """);
+                num = entrada.nextInt();
+
+
+                if(num == 0){
+                    break;
+                }
+                listaNum.add(num);
+                contador++;
+
+            }catch (InputMismatchException ex){
+                System.out.println("Debe de ser un número entero\nNo valen letras");
+                entrada.nextLine();
+            }
+
+        }while(num != 0);
+
+        System.out.println("Contenido de la lista: " + listaNum);
+        entrada.close();
     }
 }
